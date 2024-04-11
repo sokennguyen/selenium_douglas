c6ba005b0c9e26d749391482a00be46c50ec47b6
soken <nsongkien@gmail.com>
2024-04-11 15:10:04 +0300

    get all attributes

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const {By,Builder, WebDriverWait} = require('selenium-webdriver');
-const fs = require('fs');
-
-
+const { By, Builder } = require("selenium-webdriver");
+const fs = require("fs");
 
 (async function helloSelenium() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let driver = await new Builder().forBrowser("chrome").build();
 
-  await driver.get('https://www.douglas.de/de/c/parfum/01'); 
+  await driver.get("https://www.douglas.de/de/c/parfum/01");
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.css(".uc-list-button__accept-all"));
-  let cookieButton = await driver.findElement(By.css('.uc-list-button__accept-all')).getText();
 
-  await driver.findElement(By.css('.uc-list-button__accept-all')).click();
+  await driver
+    .findElement(By.css(".uc-list-button__accept-all"))
+    .click()
+    .then(() => {
+      console.log("Button clicked");
+    });
 
+  let cards = await driver.findElements(
+    By.css("a.link.product-tile__main-link"),
+  );
 
-  console.log(cookieButton);
-  fs.writeFileSync("./test.html",cookieButton) ;
+  const cardPromises = cards.map(async (card) => {
+    const innerHTML = await card.getAttribute("innerHTML");
+    console.log(innerHTML);
+  });
 
+  await Promise.all(cardPromises);
 
-      
   await driver.quit();
 })();
