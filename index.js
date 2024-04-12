6e6e9b8293a6cd3471a7914ccd7c9ebc52534352
soken <nsongkien@gmail.com>
2024-04-12 17:38:39 +0300

    handled with string splitting

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const fs = require("fs");
   //so perfume page only have 51 pages, not 136 like they say on the page number
 
   //access site and wait for cookie to pop up
-  await driver.get("https://www.douglas.de/de/c/parfum/01?page=51");
-  await driver.manage().setTimeouts({ implicit: 10000 });
+  await driver.get("https://www.douglas.de/de/c/parfum/01?page=1");
+  await driver.manage().setTimeouts({ implicit: 20000 });
 
   //accept cookie
   await driver
@@ -20,38 +20,52 @@ const fs = require("fs");
     });
 
   let cardObjs = [];
-  //insert data into card object
-  const insertCardData = async (data, field) => {
-    const promisses = data.map(async (card, index) => {
-      const content = await card.getAttribute("innerHTML");
-      cardObjs[index] = {
-        ...cardObjs[index],
-        //field name is dynamic
-        [field]: content,
-      };
-      console.log(cardObjs[index]);
-    });
-    await Promise.all(promisses);
-    console.log(field + " is scraped");
-  };
-
   //find cards and extract brand
-  let brands = await driver.findElements(
-    By.css("a.link.product-tile__main-link .top-brand"),
+  let cards = await driver.findElements(
+    By.css("a.link.product-tile__main-link "),
   );
-  await insertCardData(brands, "brand");
+  const promisses = cards.map(async (card, index) => {
+    const content = await card.getAttribute("innerHTML");
+
+    if (index === 0) console.log(content);
+
+    //split brand
+    const brand = content.split("top-brand")[1].split(">")[1].split("<")[0];
+
+    //split brand-line
+    let brandLine = "";
+    //a product either have a 'brand-line' or a 'name'
+    if (content.indexOf("brand-line") >= 0) {
+      brandLine = content.split("brand-line")[1].split(">")[1].split("<")[0];
+    } else {
+      brandLine = content.split("name")[1].split(">")[1].split("<")[0];
+    }
+
+    //split price
+    const priceStr = content.substring(
+      content.lastIndexOf("price-row__price") + 0,
+      content.lastIndexOf("€"),
+    );
+    const price = priceStr.split('product-price__price">')[1].split("&")[0];
+
+    console.log(index + " " + brand + " " + brandLine + " " + price);
+    cardObjs[index] = {
+      brand: brand,
+      "brand-line": brandLine,
+      price: price,
+    };
+  });
+  await Promise.all(promisses);
 
   //extract price
   let brandLine = await driver.findElements(
     By.css("a.link.product-tile__main-link .brand-line"),
   );
-  await insertCardData(brandLine, "brand-line");
 
   //extract price
   let prices = await driver.findElements(
     By.css("a.link.product-tile__main-link .price-row__price--discount"),
   );
-  await insertCardData(prices, "price");
 
   //turn cards into json string
   let products = {
